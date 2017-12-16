5b03a09274a988cdc6063951a663cc92ce9dbe6d
Ronny Perez <ronnypdev@gmail.com>
2017-12-15 20:53:00 -0500

    Vue js functionaility implemented

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 var app = new Vue({
 
-  
+  el: '#app',
+  data: {
+     name: 'Ronny Perez',
+     username: 'ronperez',
+     content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.',
+     css: 'css',
+     rwd: 'responsive',
+     time: '11:09 PM - 1 Jan 2016'
+  }
 
 });
